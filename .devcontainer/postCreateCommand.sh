07c7042ab44eb5df3732bcd6370754f37fd933b9
n-pugliesi <103213305+n-pugliesi@users.noreply.github.com>
2024-10-28 19:46:52 +0000

    Pending changes exported from your codespace

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,15 @@
+PORT=80
+OUTPUT_FILE="$(dirname "$(readlink -f "$0")")/../.wp-env.override.json"
+
+cat <<EOF > $OUTPUT_FILE
+{
+    "config" : {
+        "WP_HOME" : "https://${CODESPACE_NAME}-${PORT}${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}",
+        "WP_SITEURL" : "https://${CODESPACE_NAME}-${PORT}${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
+	}
+}
+EOF
+
+npm install
+composer install
+npm run up
